e277c0b2757145ee61b069d957841c8b61f0788b
Altaaf Jahankeer <ajahankeer@ryerson.ca>
2023-11-10 19:41:44 -0500

    Final Homepage 1080p Revised

--- sixplore/src/pages/homepage.js
+++ sixplore/src/pages/homepage.js
@@ -2,12 +2,15 @@ import React from "react";
 import Navbar from "../components/navbar/nav";
 import city from "../assets/citypic.svg";
 import './homepage.css';
-import { Button } from '../components/button/Button';
+import { Button } from '../components/button/Button'
+import Section from "../components/section/section";
 
 export default function HomePage() {
   return (
     <div>
+
       <Navbar/>
+    
       <div className="full">
       
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap"></link>
