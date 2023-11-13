bc10f096e2eb2fa39a1560cb5ebc82029a0f7b98
Yadu-M <ymadhu@torontomu.ca>
2023-11-12 22:26:19 -0500

    Hooked up Sign Up with Backend / Fixed errors / Cleanup

--- sixplore/util.js
+++ sixplore/util.js
@@ -1,4 +1,12 @@
+// GLOBAL CONSTANTS
+const MAX_DUMMY_USERS_COUNT = 25;
+const DATABASE_NAME = "test";
+// This number determines how many plans DUMMY user can have
+// Should be less than the dummy exploration data count which is currently 23
+const MAX_DUMMY_USER_PLAN_COUNT = 15; 
 
+
+// Some UTIL functions
 /**
  * @param {number} max Upper bound to be generated
  * @returns random number between 0 and max
@@ -7,4 +15,10 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-module.exports = getRandomInt
+
+module.exports = { 
+    getRandomInt,
+    USERS_COUNT: MAX_DUMMY_USERS_COUNT,
+    DATABASE_NAME,
+    MAX_USER_PLAN_COUNT: MAX_DUMMY_USER_PLAN_COUNT
+}
