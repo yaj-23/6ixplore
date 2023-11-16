fa61cad62b4d9f90024dc59178b62ec88ec79996
Erio Hoti <erio.hoti@ryerson.ca>
2023-11-15 23:28:17 -0500

    Link dashboard with backend
    Signed-off-by: Erio Hoti <erio.hoti@ryerson.ca>

--- sixplore/src/pages/dashboard.js
+++ sixplore/src/pages/dashboard.js
@@ -1,56 +1,87 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './dashboard.css'
 import Navbar from "../components/navbar/nav";
 import Section from "../components/section/section";
 import unlike from '../assets/x.svg'
 import like from '../assets/heart.svg'
 
-// TEMPORARY! REMOVE LATER!
-const events = [
-  {
-    name: "EXAMPLE EVENT",
-    location: "123 One Piece Avenue, Konoha A1B2C3",
-    genres: ["bar/club", "fast food", "fine dining", "escape room", "physical activity"]
-  },
-]
 
 export default function Dashboard() {
+  let [events, setEvents] = useState(null);
+  let [currentEvent, setCurrentEvent] = useState(0);
+  let [transitionLike, setTransitionLike] = useState(false);
+  let [transitionUnlike, setTransitionUnlike] = useState(false);
+
+  useEffect(() => {
+    const url = "http://localhost:3000/explore";
+
+    const fetchEvents = async () => {
+      try {
+          const response = await fetch(url);
+          const json = await response.json();
+          setEvents(json);
+          //console.log(events);
+      } catch (error) {
+          console.log("error", error);
+      }
+      };
+
+      fetchEvents();
+  }, [transitionLike, transitionUnlike]);
+
+  const showNextEvent = (isLike) => {
+    setCurrentEvent((currentEvent) => (currentEvent + 1) % events.length);
+    if (isLike) {
+      setTransitionLike(true);
+      setTimeout(() => {
+        setTransitionLike(false);
+      }, 400);
+    } else {
+      setTransitionUnlike(true);
+      setTimeout(() => {
+        setTransitionUnlike(false);
+      }, 400);
+    }
+  };
+
+  //console.log(events);
   return (
     <>
       <Navbar />
       <Section>
-        <div className="dashboard-con">
-          <div className="dashboard-button" >
+        <div className={`dashboard-con ${transitionLike || transitionUnlike ? 'transition' : ''}`}>
+          <div className={`dashboard-button unlike ${transitionUnlike ? 'transitionUnlike' : ''}`} onClick={() => showNextEvent(false)}>
             <img src={unlike} alt=""/>
           </div>
           <div className="dashboard-image">
           </div>
-          <div className="dashboard-button">
+          <div className={`dashboard-button like ${transitionLike ? 'transitionLike' : ''}`} onClick={() => showNextEvent(true)}>
             <img src={like} alt=""/>
           </div>
         </div>
         <div className="dashboard-description">
           {/* Here we'll map the events from the DB.. event genres are just text so thats what they'll take in when mapping the event genres */}
-          {events.map(({name, location, genres, rating}) => (
+          {console.log(events)}
+          {events && events.length >0 && (
             <>
-            <h1>{name}</h1>
-            <div>{location}</div>
+            <h1>{events[currentEvent].name}</h1>
+            <div>{events[currentEvent].location}</div>
             <div className="dashboard-ratings">
-            {[...Array(5)].map(() => {        
+            {[...Array(Math.round(events[currentEvent].stars))].map(() => {        
               return (         
                 <div className="star">&#9733;</div>        
               );
             })}
             </div>
             <div>
-            {genres.map((d) => (
+            {events[currentEvent].tags.map((d) => (
               <>
                 <button><b>{d}</b></button>
               </>
             ))}
             </div>
             </>
-          ))}
+          )}
         </div>
       </Section>
     </>
